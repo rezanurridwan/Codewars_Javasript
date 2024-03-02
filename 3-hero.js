aabd2ab789537ce2a662c9b119b1b5d8ec1866f6
reza nur ridwan <jeypecker01@gmail.com>
2024-03-02 10:12:16 +0700

    day 3

---
+++ 3-hero.js
@@ -0,0 +1,13 @@
+//cara pertama
+// function hero(bullets, dragons){
+//     //Get Coding!
+//     if (bullets /2 >= dragons){
+//         return true
+//     }else {
+//         return false
+//     }
+//     }
+// console.log(hero(10,5));
+//cara kedua
+const hero = (bullets, dragons) => bullets /2 >= dragons;
+console.log(hero(100,50));
